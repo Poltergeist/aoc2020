c6772258775c43e09a2c537ce520838a1210480d
Poltergeist <spectator.networx@gmail.com>
2020-12-15 07:11:54 +0100

    feat(day15): solve puzzle 2

---
+++ 155-rambunctious-efficient.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := []int{0,12,6,13,20,1,17}
+	s := map[int][]int{}
+
+	for k, v := range input {
+		s[v] = []int{k}
+	}
+
+	l := input[len(input)-1]
+	for c := len(input); c < 30000000 ; c++ {
+		if len(s[l]) == 1 {
+			s[0] = append([]int{c}, s[0]...)[:2]
+			l = 0
+			continue
+		}
+		n := s[l][0] - s[l][1]
+		if len(s[n]) == 0 {
+			s[n] = []int{c}
+			l = n
+			continue
+		}
+		s[n] = []int{c, s[n][0]}
+		l = n
+	}
+	fmt.Println(l)
+
+}
