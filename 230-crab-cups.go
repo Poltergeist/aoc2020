64a2051742a8403b44c9bebd7dcd75d1bee77e4d
Poltergeist <spectator.networx@gmail.com>
2020-12-23 17:14:55 +0100

    feat(day23): solve puzzle 1

--- 230-crab-cups.go
+++ 230-crab-cups.go
@@ -19,7 +19,7 @@ func main() {
 	input := []int{}
 	low := 10000000
 	high := 0
-	for _, v := range strings.Split("389125467", "") {
+	for _, v := range strings.Split("942387615", "") {
 		n := func(i string) int {
 			xi, _ := strconv.Atoi(i)
 			return xi
@@ -31,18 +31,17 @@ func main() {
 			high = n
 		}
 		input = append(input, n)
-
 	}
 
 	p := 0
-	for x := 1; x <= 10; x++ {
+	for x := 1; x <= 100; x++ {
 		l := input[p]
 		t := append([]int{}, input[p+1:p+4]...)
 		input = append(input[:p+1], input[p+4:]...)
 		d := l - 1
 		for Index(input, d) == -1 {
 			d--
-			if d <= low {
+			if d < low {
 				d = high
 			}
 
@@ -52,12 +51,6 @@ func main() {
 
 		input = append(input[:di+1], append(t, input[di+1:]...)...)
 
-		fmt.Printf(`-- move %d --
-label: %d
-cups: %v
-pick up: %v
-destination: %d
-`, x, l, input, t, d)
 		p = Index(input, l) + 1
 		if p >= len(input) {
 			p = 0
@@ -65,10 +58,14 @@ destination: %d
 		if p+4 >= len(input) {
 			input = append(input[3:], input[:3]...)
 			p = Index(input, l) + 1
+
 		}
 
 	}
-	fmt.Println(input)
 	io := Index(input, 1)
-	fmt.Println(append(input[io+1:], input[:io]...))
+	b := ""
+	for _, v := range append(input[io+1:], input[:io]...) {
+		b += strconv.Itoa(v)
+	}
+	fmt.Println(b)
 }
